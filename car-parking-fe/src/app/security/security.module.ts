ec8b4eef665b47d4782717c69e46c26059c8d0c6
quynhngo2909 <dieuquynh105@gmail.com>
2023-03-30 20:15:53 +0700

    QuynhND add module security

--- car-parking-fe/src/app/security/security.module.ts
+++ car-parking-fe/src/app/security/security.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoginRoutingModule } from './login-routing.module';
+import { SecurityRoutingModule } from './security-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    SecurityRoutingModule
   ]
 })
-export class LoginModule { }
+export class SecurityModule { }
