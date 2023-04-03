c2f4505f521ea3318cb16f090be97426692d7d0d
xuanbao31 <baohxgbd210054@fpt.edu.vn>
2023-04-03 16:59:32 +0700

    update

--- car-parking-fe/src/app/model/ilocation.ts
+++ car-parking-fe/src/app/model/ilocation.ts
@@ -1,14 +1,14 @@
-import {Floor} from "./floor";
-import {Section} from "./section";
+import {Floor} from './floor';
+import {Section} from './section';
 
 export interface ILocation {
-  id: number;
-  name: string;
-  isOccupied: boolean;
-  width: number;
-  height: number;
-  length: number;
-  permissionCarTypeLocations: string;
-  floor: Floor;
-  section: Section;
+  id?: number;
+  name?: string;
+  isOccupied?: boolean;
+  width?: number;
+  height?: number;
+  length?: number;
+  permissionCarTypeLocations?: string;
+  floor?: Floor;
+  section?: Section;
 }
--- car-parking-fe/src/app/service/location.service.ts
+++ car-parking-fe/src/app/service/location.service.ts
@@ -1,6 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {ILocation} from '../model/ilocation';
+// @ts-ignore
+import {Page} from 'ngx-pagination/dist/pagination-controls.directive';
 
 const API_URL = `${environment.apiUrl}`;
 
@@ -11,4 +15,38 @@ export class LocationService {
 
   constructor(private http: HttpClient) {
   }
+
+  getAllLocation(search: string, page: number): Observable<Page> {
+    // alert(search)
+    return this.http.get<Page>('http://localhost:8080/location/list?search=' + search + '&page=' + page);
+  }
+
+  deleteLocation(id: number): Observable<Page> {
+    return this.http.delete<Page>('http://localhost:8080/location/delete/' + id);
+  }
+}
+export interface Page<T> {
+  content: T[];
+  pageable: {
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    pageNumber: number;
+    pageSize: number;
+    offset: number;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
 }
