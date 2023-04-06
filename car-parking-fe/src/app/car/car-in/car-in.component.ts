2120757799d6bd5dbe345af72619fed27487c91c
nguyenminhhoang291998 <nguyenminhhoang291998@gmail.com>
2023-04-06 11:57:21 +0700

    add

---
+++ car-parking-fe/src/app/car/car-in/car-in.component.ts
@@ -0,0 +1,201 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {CarInOutService} from '../../service/car-in-out.service';
+import {ICarInOut} from '../../model/i-car-in-out';
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-car-in',
+  templateUrl: './car-in.component.html',
+  styleUrls: ['./car-in.component.css']
+})
+export class CarInComponent implements OnInit {
+  dataList: ICarInOut[];
+  plateNumberImage?: File;
+  carIn?: ICarInOut;
+  timeIn = '';
+  now: any;
+  urlCarInImage = '../../../../assets/car-images/default.png';
+  @ViewChild('uploadFile', {static: true}) public avatarDom: ElementRef | undefined;
+
+  constructor(private carInOutService: CarInOutService,
+              private router: Router,
+              private storage: AngularFireStorage
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.searchCarIn('', '', '');
+  }
+
+  saveCarIn() {
+    debugger
+    // car's data not found
+    if (this.carIn == null) {
+      let timerInterval;
+      Swal.fire({
+        title: 'Vui lòng tìm xe trước khi lưu!',
+        icon: 'warning',
+        html: 'Tự động đóng trong <b></b> ms.',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const b = Swal.getHtmlContainer().querySelector('b');
+          timerInterval = setInterval(() => {
+            b.textContent = String(Swal.getTimerLeft());
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        }
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+          console.log('I was closed by the timer');
+        }
+      });
+      return;
+    }
+    const carInSaved = {
+      timeIn: this.timeIn,
+      carDTO: {
+        id: this.carIn.carId
+      },
+      urlCarInImage: this.urlCarInImage
+    };
+    console.log(carInSaved.timeIn);
+    this.carInOutService.saveCarIn(carInSaved).subscribe(() => {
+      Swal.fire({
+        title: 'Lưu thành công!',
+        text: 'Xin mời xe vào!',
+        icon: 'success',
+        confirmButtonText: 'Xác nhận',
+        confirmButtonColor: 'darkorange'
+      });
+      this.carIn = null;
+      this.urlCarInImage = '../../../../assets/car-images/default.png';
+      this.timeIn = '';
+      // setTimeout(() => {
+      //   window.location.reload();
+      // }, 3000);
+    }, error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Lỗi hệ thống',
+        text: 'Vui lòng liên nhân viên kĩ thuật!',
+        confirmButtonText: 'Xác nhận',
+        confirmButtonColor: 'darkorange'
+      });
+    });
+  }
+
+  onUpload(event) {
+    this.plateNumberImage = event.target.files[0];
+    if (this.plateNumberImage != null) {
+      const filePath = this.plateNumberImage.name;
+      const fileRef = this.storage.ref(filePath);
+
+      const imageFormData = new FormData();
+      imageFormData.append('plateNumberImage', this.plateNumberImage, this.plateNumberImage.name);
+      this.carInOutService.searchCarInByScanning(imageFormData).subscribe(carIn => {
+        console.log(carIn);
+        this.carIn = carIn;
+        const now = new Date();
+        this.storage.upload(filePath, this.plateNumberImage).snapshotChanges().pipe(
+          finalize(() => (fileRef.getDownloadURL().subscribe(url => {
+            this.urlCarInImage = url;
+          })))
+        ).subscribe();
+        // for display and save
+        const options = {
+          timeZone: 'Asia/Ho_Chi_Minh',
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hour12: false
+        };
+        this.timeIn = now.toLocaleString('vi-VN', options);
+      }, error => {
+        console.log(error);
+        // car not existing in database
+        if (error.status === 404) {
+          Swal.fire({
+            title: 'Không tìm thấy xe',
+            text: 'Xe có thể đã hết vé!',
+            icon: 'question',
+            confirmButtonText: 'Xác nhận',
+            confirmButtonColor: 'darkorange'
+          });
+          this.timeIn = '';
+        }
+        // the system unable to scan the image
+        if (error.status === 406) {
+          Swal.fire({
+            title: 'Không quét được biển số',
+            text: 'Vui lòng ấn vào tìm xe',
+            icon: 'error',
+            confirmButtonText: 'Xác nhận',
+            confirmButtonColor: 'darkorange'
+          });
+          this.timeIn = '';
+        }
+        // the system error or the image not identified
+        if (error.status === 500) {
+          Swal.fire({
+            title: 'Lỗi hệ thống!',
+            text: 'Không nhận được file hoặc hệ thống trục trặc, vui lòng thử cách khác!',
+            icon: 'error',
+            confirmButtonText: 'Xác nhận',
+            confirmButtonColor: 'darkorange'
+          });
+          this.timeIn = '';
+        }
+      });
+    } else {
+      Swal.fire({
+        title: 'Tập tin không hợp lệ',
+        text: 'Vui lòng tải lại',
+        icon: 'error',
+        confirmButtonText: 'Xác nhận',
+        confirmButtonColor: 'darkorange'
+      });
+    }
+  }
+
+  searchCarIn(customerName: string, customerPhoneNumber: string, carPlateNumber: string) {
+    this.carInOutService.searchCarIn(customerName, customerPhoneNumber, carPlateNumber).subscribe(carInList => {
+      console.log(carInList);
+      this.dataList = carInList;
+    });
+  }
+
+  selectCar(carId: number) {
+    debugger
+    for (let i = 0; i < this.dataList.length; i++) {
+      if (this.dataList[i].carId == carId) {
+        this.carIn = this.dataList[i];
+        return;
+      }
+    }
+    const options = {
+      timeZone: 'Asia/Ho_Chi_Minh',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false
+    };
+    const now = new Date();
+    this.timeIn = now.toLocaleString('vi-VN', options);
+  }
+}
---
+++ car-parking-fe/src/app/employee/employee-list/employee-list.component.ts
@@ -0,0 +1,146 @@
+import {Component, OnInit} from '@angular/core';
+import {EmployeeService} from '../../service/employee.service';
+import {Employee} from '../../model/employee';
+import Swal from 'sweetalert2';
+import {log} from "util";
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[] = [];
+  employee: any;
+  nameSearch: string;
+  // @ts-ignore
+  employeeDelete: Employee = {};
+  startDate: string;
+  endDate: string;
+  street: string;
+  message: '';
+  size = 5;
+  province: number = 0;
+  provinceList: any;
+  pageCount = 0;
+  pageNumbers: number[] = [];
+  totalPages = 0;gi
+  // số trang hiện tại
+  currentPage = 0;
+
+  constructor(private employeeService: EmployeeService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAll();
+    this.employeeService.getAllProvince().subscribe(next => {
+      this.provinceList = next.data.data;
+    });
+  }
+  getProvince(value: string) {
+    console.log(value);
+    if (!isNaN(parseInt(value))) {
+      this.province = parseInt(value);
+    } else {
+      this.province = 0;
+    }
+  }
+
+  getAll() {
+    this.message = null;
+    this.employeeService.getAllEmployee(this.currentPage, this.size,
+      this.nameSearch, this.startDate, this.endDate, this.street, this.province).subscribe(data => {
+      this.employee = data;
+      this.employees = this.employee.content;
+      this.pageCount = this.employee.totalPages;
+      this.pageNumbers = Array.from({length: this.pageCount}, (v, k) => k + 1);    }, error => {
+      // this.message = error.error;
+      Swal.fire(
+        'Không tìm thấy dữ liệu!',
+        '',
+        'error'
+      );
+    });
+  }
+
+  delete(id: number) {
+    if (id != null) {
+      return this.employeeService.deleteById(this.employeeDelete.id).subscribe(data => {
+        Swal.fire(
+          'Xóa nhân viên thành công',
+          '',
+          'success'
+        );
+        this.getAll();
+      }, error => {
+        Swal.fire(
+          'Xóa nhân viên không thành công',
+          '',
+          'error'
+        );
+      });
+    }
+  }
+
+  get pageNumbersToDisplay() {
+    const currentPageIndex = this.currentPage;
+    const totalPageCount = this.pageCount;
+    const pagesToShow = 3;
+
+    if (totalPageCount <= pagesToShow) {
+      return Array.from({ length: totalPageCount }, (_, i) => i + 1);
+    }
+
+    const startPage = Math.max(0, currentPageIndex - Math.floor(pagesToShow / 2));
+    let endPage = startPage + pagesToShow - 1;
+
+    if (endPage >= totalPageCount) {
+      endPage = totalPageCount - 1;
+    }
+
+    let pageNumbersToDisplay: (number | string)[] = Array.from({ length: endPage - startPage + 1 }, (_, i) => i + startPage + 1);
+
+    if (startPage > 0) {
+      pageNumbersToDisplay = [ '...', ...pageNumbersToDisplay];
+    }
+
+    if (endPage < totalPageCount - 1) {
+      pageNumbersToDisplay = [...pageNumbersToDisplay, '...'];
+    }
+
+    return pageNumbersToDisplay;
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+    this.getAll();
+  }
+
+  nextPage() {
+    if (this.currentPage < this.pageCount - 1) {
+      this.currentPage++;
+    }
+    this.getAll();
+  }
+
+  goToPage(pageNumber: number | string) {
+    if (typeof pageNumber === 'number') {
+      this.currentPage = pageNumber - 1;
+    }
+    this.getAll();
+  }
+
+  clearInputs() {
+    this.nameSearch = '';
+    this.startDate = '';
+    this.endDate = '';
+    this.street= '';
+    this.province = 0
+    this.getAll()
+  }
+
+}
