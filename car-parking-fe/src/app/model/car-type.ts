c2a7e30eaa533a8788ab5fc36487f656657a482b
lehuutai0310 <lehuutai0310@gmail.com>
2023-04-06 18:48:47 +0700

    update

--- car-parking-fe/src/app/model/car-type.ts
+++ car-parking-fe/src/app/model/car-type.ts
@@ -1,11 +1,8 @@
 export interface CarType {
-<<<<<<< HEAD
   id: number;
   name: string;
   rate: number;
-=======
   id?:  number;
   name?: string;
   rate?: number;
->>>>>>> origin/car-in-out
 }
